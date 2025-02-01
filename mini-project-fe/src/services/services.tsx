1d037bf99469cc379b9b9c838082e99c4c63f36a
Dev-Api-DeLeon <kin.deleon@mlhuillier.com>
2025-02-01 15:43:59 +0800

    [fixing .env config]

--- mini-project-fe/src/services/services.tsx
+++ mini-project-fe/src/services/services.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Define your API base URL
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Define a function to create an instance of axios with some defaults
 const axiosInstance = axios.create({
